f016aa96999755eabec241a1cc2b3a883f159fd1
Daniel <gabrielang56799@gmail.com>
2026-06-13 20:25:14 +0530

    updated ui, fix chatTitle ui

--- Frontend/src/feature/chat/pages/Dashboard.jsx
+++ Frontend/src/feature/chat/pages/Dashboard.jsx
@@ -6,7 +6,7 @@ import './dashboard.scss'
 import Markdown from 'react-markdown';
 import { setCurrentChatId } from '../chat.slice.js';
 import { useDispatch } from 'react-redux';
-import { RiSendPlaneFill } from "@remixicon/react";
+import {RiCustomerServiceLine, RiSettings3Line, RiSendPlaneFill, RiChatNewLine, RiArrowRightSLine, RiChatAi3Fill } from "@remixicon/react";
 
 
 
@@ -16,8 +16,10 @@ const Dashboard = () => {
   const chat = useChat()
   const [messages, setMessages] = useState('')
   const [chatInput, setChatInput] = useState('')
+  const [activeChatId, setActiveChatId] = useState(null);
   
   const chats = useSelector(state=>state.chat.chats)
+  const user = useSelector(state=>state.auth.user)
   const currentChatId = useSelector(state=>state.chat.currentChatId)
   
 
@@ -43,6 +45,7 @@ const handleSubmitMessage = (e)=>{
 }
 const handleChatClick = (chatId) =>{
   chat.handleGetMessages(chatId)
+  setActiveChatId(chatId)
 }
   return (
     <div className="dashboard">
@@ -50,24 +53,61 @@ const handleChatClick = (chatId) =>{
         <h1>SeekAI</h1>
         <div className="createNewChat">
           <button onClick={()=>dispatch(setCurrentChatId(null))} className='createNewChatBtn'>
-            Create New Chat
+            New Chat
+            <RiChatNewLine
+            size={20}
+            opacity={0.5} />
           </button>
         </div>
+        
+        <div className="conversationTitle">
+            <RiChatAi3Fill 
+            size={20}/>
+            <h6>Conversation</h6>
+          </div>
         <div className="titleList">
-  {Object.values(chats).map((chat) => (
-    <h4  key={chat.id} onClick={() => handleChatClick(chat.id)}>
-      <ReactMarkdown>
-      {chat.title}
-      </ReactMarkdown>
-    </h4>
-  )).reverse()}
-</div>
+           {Object.values(chats).map((chat) => (
+             <h4 key={chat.id}
+              style={{opacity:activeChatId == chat.id ?1:0.6}}
+              onClick={() => handleChatClick(chat.id)}>
+               <ReactMarkdown>
+               {chat.title}
+               </ReactMarkdown>
+             </h4>
+           )).reverse()}
+        </div>
+
+        <div className="preferencesPanel">
+          <div className="support box">
+            <RiCustomerServiceLine />
+            <span>Support</span>
+
+          </div>
+          <div className="setting box">
+            <RiSettings3Line />
+            <span>Setting</span>
+          </div>
+
+        </div>
+
+        <div className="userDetail">
+          <div className="profileImg">
+            <img src="https://png.pngtree.com/png-vector/20250512/ourmid/pngtree-default-avatar-profile-icon-gray-placeholder-vector-png-image_16213764.png" 
+            loading='lazy'
+            alt="profilePicture" />
+            <h5>{user.username}</h5>
+          </div>
+          <RiArrowRightSLine />
+        </div>
       </div>
 
       <div className="chatContainer">
         <div className="messages" aria-live="polite">
-          {chats[currentChatId]?.messages?.length === 0 ? (
-            <div className="empty">No messages yet. Start the conversation below.</div>
+          {chats[currentChatId]?.messages?.length === 0 || currentChatId == null ? (
+            <div className="empty">
+              <h4>What are you seeking today?</h4>
+              <p>One prompt away from clarity.</p>
+              </div>
           ) : (
             chats[currentChatId]?.messages?.map((m) => (
               <div key={m.id} className={`message ${m.role}`}>
