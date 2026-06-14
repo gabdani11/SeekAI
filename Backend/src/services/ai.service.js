e7f26f662de7b9c6c0700deb14e429049fb17a98
Daniel <gabrielang56799@gmail.com>
2026-06-14 21:17:51 +0530

    fixed systemMessage

--- Backend/src/services/ai.service.js
+++ Backend/src/services/ai.service.js
@@ -56,9 +56,10 @@ export async function generateChatTitle(message)
 {
     const response = await model2.invoke([
         new SystemMessage(`You are a helpful assistant that generates a concise title for a conversation based on the following message.
-            User will provide a message, and you will generate a title that captures the essence of the conversation in a few words. The title should be clear, relevant, and engaging.`),
+            User will provide a message, and you will generate a title that captures the essence of the conversation in a few words. The title should be clear, relevant, and engaging.
+            title shoud be in sentence case, and should not exceed 6 words.`),
         new HumanMessage(`
-            generate a title for the following message: ${message}`)
+            generate a title for the following message : ${message}`)
     ])
     return response.text
 }
